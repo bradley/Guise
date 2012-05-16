6e85b18b43390ec990938c080a3946f81168d7d2
Bradley Griffith <bradley.j.griffith@gmail.com>
2012-05-16 11:27:38 -0500

    blargh

--- app.rb
+++ app.rb
@@ -5,25 +5,7 @@ require 'rack'
 require_relative 'minify_resources'
 
 class App < Sinatra::Application
-  enable :sessions
 
-  configure :production do
-	set :clean_trace, true
-	set :css_files, :blob
-	set :js_files,  :blob
-	MinifyResources.minify_all
-  end
-
-  configure :development do
-	set :css_files, MinifyResources::CSS_FILES
-	set :js_files,  MinifyResources::JS_FILES
-  end
-
-  helpers do
-	#This includes a set of methods provided by Rack. We now have access to a h() method to escape HTML.
-	include Rack::Utils
-	alias_method :h, :escape_html
-  end
 end
 
 require_relative 'helpers/init'
