767e1466de59153200ad8cc39045de9aaaa3b74e
Bradley Griffith <bradley.j.griffith@gmail.com>
2012-05-16 11:53:40 -0500

    silencio

--- routes/main.rb
+++ routes/main.rb
@@ -1,7 +1,26 @@
 # encoding: utf-8
 class App < Sinatra::Application
-  get '/' do
-  	@title = 'something'
-    erb :home
+  SITE_NAME = "Site Name"
+  DEFAULT_TITLE = "Default Title"
+
+  before do
+	@title = DEFAULT_TITLE
+  end
+
+  not_found do
+  	redirect '/404'
+  end
+
+  ['/', '/home', '/home/'].each do |path|
+	get path do
+	  erb :home
+	end
+  end
+
+  ['/404', '/404/'].each do |path|
+	get path do
+	  @title = '404'
+	  erb :unfound
+	end
   end
 end
