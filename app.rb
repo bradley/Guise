2b9fbe9c9ff6e5cddc15c8107ceaa6653a4224f3
Bradley Griffith <bradley.j.griffith@gmail.com>
2012-05-16 11:29:00 -0500

    blargh

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@
 require 'sinatra'
 require 'rack'
 
-require_relative 'minify_resources'
+#require_relative 'minify_resources'
 
 class App < Sinatra::Application
-
+#
 end
 
-require_relative 'helpers/init'
+#require_relative 'helpers/init'
 require_relative 'routes/init'
