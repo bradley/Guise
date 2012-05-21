8ae698e639ac8b860cd244ebdf2446284d10eac3
Bradley Griffith <bradley.j.griffith@gmail.com>
2012-05-21 18:55:07 -0500

    hi there

--- routes/ajax.rb
+++ routes/ajax.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Application
 
     #@user = User.new unless @user
 
-    @tester = Tester.create
+    @tester = Tester.new
     @tester.username = 'ted'
     if @tester.save
       return 'asd'
