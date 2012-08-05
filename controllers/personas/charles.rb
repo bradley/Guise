b48aba7ec0eaa2e14cc52e22ea400f19a6e1d8a2
Bradley Griffith <bradley@wieck.com>
2012-08-04 19:35:53 -0500

    testing things i dont know

--- controllers/personas/charles.rb
+++ controllers/personas/charles.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 class CharlesTaylor
 	def speak!
-		"The same public discussion is deemed to pass through our debate today, and someone else’s earnest conversation tomorrow, and the newspaper interview Thursday and so on… ."
+		"The same public discussion is deemed to pass through our debate today, and someone else’s earnest conversation tomorrow, and the newspaper interview Thursday and so on…"
 	end
 end
